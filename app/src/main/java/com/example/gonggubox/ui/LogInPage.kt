454b06084013c86c02d379a369ae88462a6e9eda
kanukim97 <angry_ryan@kakao.com>
2023-09-21 22:46:30 +0900

    [Commit]  - LogInPage.kt Button

--- app/src/main/java/com/example/gonggubox/ui/LogInPage.kt
+++ app/src/main/java/com/example/gonggubox/ui/LogInPage.kt
@@ -3,16 +3,23 @@ package com.example.gonggubox.ui
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.ShapeDefaults
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -29,7 +36,9 @@ fun LogInPage() {
         color = MaterialTheme.colorScheme.surface
     ) {
         Column(
-            modifier = Modifier.wrapContentHeight().fillMaxWidth(),
+            modifier = Modifier
+                .wrapContentHeight()
+                .fillMaxWidth(),
             verticalArrangement = Arrangement.Top,
             horizontalAlignment = Alignment.Start,
             content = {
@@ -56,6 +65,30 @@ fun LogInPage() {
                             fontSize = 40.sp,
                             fontWeight = FontWeight.Bold,
                             fontFamily = Pretendard_Kr
+                        )
+                    }
+                )
+                Column(
+                    modifier = Modifier.wrapContentHeight().fillMaxWidth(),
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    content = {
+                        Button(
+                            onClick = { /*TODO*/ },
+                            modifier = Modifier
+                                .width(320.dp)
+                                .height(50.dp)
+                                .clip(ShapeDefaults.ExtraSmall),
+                            content = {  }
+                        )
+                        Spacer(modifier = Modifier.size(20.dp))
+                        Button(
+                            onClick = { /*TODO*/ },
+                            modifier = Modifier
+                                .width(320.dp)
+                                .height(50.dp)
+                                .clip(ShapeDefaults.ExtraSmall),
+                            content = {  }
                         )
                     }
                 )
